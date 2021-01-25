1a55b592d95adab5a5a9cb05104ee278a52fa5b2
Joseph Key <jkey3@brookdale.com>
2021-01-25 12:44:07 -0600

    tidy

--- src/main/java/com/example/demo/VendingMachineService.java
+++ src/main/java/com/example/demo/VendingMachineService.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import java.util.concurrent.ExecutionException;
 
 public interface VendingMachineService {
+
     Product[] fetchProducts() throws ExecutionException, InterruptedException;
     Order submitOrder(String name) throws ExecutionException, InterruptedException;
+
 }
